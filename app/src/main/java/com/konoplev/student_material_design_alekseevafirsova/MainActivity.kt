b3f8edfc6e6004d5576a3644c3354781eeb8cce0
dazzes4545 <dazzes7619@gmail.com>
2026-03-25 09:11:53 +0300

    Step 6: Custom color scheme added

--- app/src/main/java/com/konoplev/student_material_design_alekseevafirsova/MainActivity.kt
+++ app/src/main/java/com/konoplev/student_material_design_alekseevafirsova/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -101,29 +102,38 @@ fun StudentItem(
     student: Student,
     modifier: Modifier=Modifier
 ) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(dimensionResource(R.dimen.padding_small))
-    )
-    {
-        StudentIcon(student.imageResourceId)
-        StudentInformation(student.name, student.age)
+    Card(modifier= modifier) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_small))
+        )
+        {
+            StudentIcon(student.imageResourceId)
+            StudentInformation(student.name, student.age)
+        }
     }
 }
 @Composable
 fun StudentApp() {
-    LazyColumn() {
-        items(students) {
-            StudentItem(student = it)
+    Scaffold { it ->
+        LazyColumn(contentPadding = it) {
+            items(students) {
+                StudentItem(
+                    student = it,
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
         }
     }
 }
-@Preview
-@Composable
-fun StudentPreview(){
-    Student_Material_Design_AlekseevaFirsovaTheme(darkTheme = false){
-        StudentApp()
+    @Preview
+    @Composable
+    fun StudentPreview() {
+        Student_Material_Design_AlekseevaFirsovaTheme(darkTheme = false) {
+            StudentApp()
+        }
     }
-}
+
 
